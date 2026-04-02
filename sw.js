38d2b4885384a29aa898cda13d8c0a32bda8d33b
edu-spuqui <edu.spuqui@gmail.com>
2026-04-02 02:53:55 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
-self.addEventListener('fetch', function(event) {
-  // This is a dummy service worker to satisfy Chrome's PWA requirements
+// sw.js
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+  
+  setInterval(() => {
+    const now = new Date();
+    // Added 'second: 2-digit' to the formatting
+    const timeString = now.toLocaleTimeString('pt-BR', { 
+        hour: '2-digit', 
+        minute: '2-digit',
+        second: '2-digit'
+    });
+    
+    self.clients.matchAll().then(clients => {
+      clients.forEach(client => {
+        client.postMessage({ type: 'CLOCK_TICK', time: timeString });
+      });
+    });
+  }, 500); 
+});
+
+self.addEventListener('fetch', (event) => {});
