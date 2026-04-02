882881982a8ddc9a00dc801eb619b4d01dd4109d
edu-spuqui <edu.spuqui@gmail.com>
2026-04-02 03:33:36 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,30 @@
-// sw.js
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
-self.addEventListener('fetch', (e) => {}); // Required for PWA
+const CACHE_NAME = 'psop-v1';
+const ASSETS = [
+  './pokertimer.html',
+  './manifest.json',
+  './icon.png',
+  './alarm.mp3',
+  'https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap'
+];
+
+// Install: Cache all the poker gear
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+// Fetch: THE IMPORTANT PART - Actually serves files from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
