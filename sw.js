2e008d2c0c3def644c0f4e32076c54dc12dcb639
edu-spuqui <edu.spuqui@gmail.com>
2026-04-02 03:15:42 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,4 @@
 // sw.js
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
-  
-  setInterval(() => {
-    const now = new Date();
-    // Added 'second: 2-digit' to the formatting
-    const timeString = now.toLocaleTimeString('pt-BR', { 
-        hour: '2-digit', 
-        minute: '2-digit',
-        second: '2-digit'
-    });
-    
-    self.clients.matchAll().then(clients => {
-      clients.forEach(client => {
-        client.postMessage({ type: 'CLOCK_TICK', time: timeString });
-      });
-    });
-  }, 500); 
-});
-
-self.addEventListener('fetch', (event) => {});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', (e) => {}); // Required for PWA
